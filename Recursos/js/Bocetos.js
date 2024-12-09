a1e5b111a81deeccadd08e4aa89f7a8bb8ca9709
Exequiel Arias <130775867+ExequielArias@users.noreply.github.com>
2024-12-08 21:14:25 -0300

    Delete Recursos directory

--- Recursos/js/Bocetos.js
+++
@@ -1,6 +0,0 @@
-import { tiendaBocetos } from "./Tienda-Bocetos.js";
-import { RenderizadoBocetos } from "./funciones.js";
-
-const contenedor = document.getElementById('contenedor-Bocetos');
-
-RenderizadoBocetos(tiendaBocetos.Bocetos, contenedor);
--- Recursos/js/Piercing-Prin.js
+++
@@ -1,5 +0,0 @@
-import { renderizarPiercing } from "./funciones.js";
-import { TiendaPiercings } from "./piercings.js";
-
-const contenedor = document.getElementById("contenedor-piercing");
-renderizarPiercing(TiendaPiercings.piercings, contenedor);
